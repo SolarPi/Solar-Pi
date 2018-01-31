75f8fd3a4660518fa1e89647a3553d4273309e0e
The-Sleepy-Penguin <33980939+The-Sleepy-Penguin@users.noreply.github.com>
2018-01-31 14:15:49 +0000

    Improvements for RPi

--- Solar Pi Power/Battery Meter v2.py
+++ Solar Pi Power/Battery Meter v2.py
@@ -1,6 +1,7 @@
 from appJar import gui
 from time import sleep
 from threading import Thread
+from PIL import Image, ImageTk
 
 def over(param):
     program.hide()
@@ -51,51 +52,77 @@ class Handler(FileSystemEventHandler):
             meter()
 
 def meter():
-    with open("../ramdisk/power", "r") as file:
+    global change
+    try:
+        with open("../ramdisk/power", "r") as file:
             data = file.readlines()
-            data = data[0]
-            data = data.split(",")
-            percent = float(data[0])
-            if data[1] == "B":
-                program.hideImage("bolt")
-                if percent >= 100:  # 100
-                    program.setLabelBg(["1", "2", "3", "4", "5", "6", "7", "8", "9"], "green")
-                elif percent > 80 and percent < 90:  # 87.5
-                    program.setLabelBg(["1", "2", "3", "4", "5", "6", "7", "8"], "green")
-                    program.setLabelBg("9", "black")
-                elif percent > 70 and percent < 80:  # 75
-                    program.setLabelBg(["1", "2", "3", "4", "5", "6", "7"], "green")
-                    program.setLabelBg(["8", "9"], "black")
-                elif percent > 55 and percent < 70:  # 62
-                    program.setLabelBg(["1", "2", "3", "4", "5", "6"], "green")
-                    program.setLabelBg(["7", "8", "9"], "black")
-                elif percent > 45 and percent < 55:  # 50
-                    program.setLabelBg(["1", "2", "3", "4", "5"], "green")
-                    program.setLabelBg(["6", "7", "8", "9"], "black")
-                elif percent > 25 and percent < 45:  # 37
-                    program.setLabelBg(["1", "2", "3", "4"], "#a5dd24")
-                    program.setLabelBg(["5", "6", "7", "8", "9"], "black")
-                elif percent > 20 and percent < 30:  # 25
-                    program.setLabelBg(["1", "2", "3"], "#ddc724")
-                    program.setLabelBg(["4", "5", "6", "7", "8", "9"], "black")
-                elif percent > 7 and percent < 17:  # 12.5
-                    program.setLabelBg(["1", "2"], "#dd6524")
-                    program.setLabelBg(["3", "4", "5", "6", "7", "8", "9"], "black")
-                elif percent > 4 and percent < 7:  # 5.5
-                    program.setLabelBg("1", "#dd4125")
-                    program.setLabelBg(["2", "3", "4", "5", "6", "7", "8", "9"], "black")
-                elif percent < 4:
-                    program.setLabelBg(["1", "2", "3", "4", "5", "6", "7", "8", "9"], "black")
-
-            elif data[1] == "C":
+        data = data[0]
+        data = data.split(",")
+        percent = float(data[0])
+        change = False
+        if data[1] == "B":
+            program.hideCanvas("canvas")
+            #program.hideLabel("flash")
+            if percent >= 100:  # 100
                 program.setLabelBg(["1", "2", "3", "4", "5", "6", "7", "8", "9"], "green")
-                program.showImage("bolt")
+            elif percent > 80 and percent < 90:  # 87.5
+                program.setLabelBg(["1", "2", "3", "4", "5", "6", "7", "8"], "green")
+                program.setLabelBg("9", "black")
+            elif percent > 70 and percent < 80:  # 75
+                program.setLabelBg(["1", "2", "3", "4", "5", "6", "7"], "green")
+                program.setLabelBg(["8", "9"], "black")
+            elif percent > 55 and percent < 70:  # 62
+                program.setLabelBg(["1", "2", "3", "4", "5", "6"], "green")
+                program.setLabelBg(["7", "8", "9"], "black")
+            elif percent > 45 and percent < 55:  # 50
+                program.setLabelBg(["1", "2", "3", "4", "5"], "green")
+                program.setLabelBg(["6", "7", "8", "9"], "black")
+            elif percent > 25 and percent < 45:  # 37
+                program.setLabelBg(["1", "2", "3", "4"], "#a5dd24")
+                program.setLabelBg(["5", "6", "7", "8", "9"], "black")
+            elif percent > 20 and percent < 30:  # 25
+                program.setLabelBg(["1", "2", "3"], "#ddc724")
+                program.setLabelBg(["4", "5", "6", "7", "8", "9"], "black")
+            elif percent > 7 and percent < 17:  # 12.5
+                program.setLabelBg(["1", "2"], "#dd6524")
+                program.setLabelBg(["3", "4", "5", "6", "7", "8", "9"], "black")
+            elif percent > 4 and percent < 7:  # 5.5
+                program.setLabelBg("1", "#dd4125")
+                program.setLabelBg(["2", "3", "4", "5", "6", "7", "8", "9"], "black")
+            elif percent < 4:
+                program.setLabelBg(["1", "2", "3", "4", "5", "6", "7", "8", "9"], "black")
 
+        elif data[1] == "C":
+            #program.setLabelBg(["4", "5", "6", "7", "8", "9"], "black")
+            #program.setLabelBg(["1", "2", "3"], "orange")
+            #program.showLabel("flash")
+            program.showCanvas("canvas")
+            change = True
+            t2 = Thread(target=label_change)
+            t2.start()
+
+    except IOError:
+        sleep(1)
+        meter()
+            
 
 
 def batt_watcher():
     w = Watcher()
     w.run()
+
+change = False
+
+def label_change():
+    global change
+    while change == True:
+        program.setLabelBg(["1", "2", "3", "4", "5", "6", "7", "8", "9"], "black")
+        for i in range(1, 10):
+            program.setLabelBg(str(i), "green")
+            sleep(1.5)
+        program.setLabelBg(["1", "2", "3", "4", "5", "6", "7", "8", "9"], "black")
+        sleep(1.5)
+        
 
 
 with gui("meter", "85x40") as program:
@@ -115,9 +142,21 @@ with gui("meter", "85x40") as program:
         program.setLabelBg("10", "grey")
         program.setLabelSticky("10", "w")
 
-        program.addImage("bolt", "../Resources/Images/lightning bolt.gif", 0, 2, colspan=5)
-        program.zoomImage("bolt", -7)
-        program.hideImage("bolt")
+##        program.addFlashLabel("flash", "", 0, 0, colspan=9)
+##        program.setLabelSticky("flash", "nesw")
+##        program.setLabelBg("flash", "green")
+##        program.hideLabel("flash")
+
+        #program.addImage("bolt", "../Resources/Images/lightning bolt.gif", 0, 2, colspan=5)
+        #program.zoomImage("bolt", -7)
+        #program.setImageSticky("bolt", "")
+        #program.setImageBg("bolt", "black") #"#21c62c"
+        #program.hideImage("bolt")
+
+        program.addCanvas("canvas", 0, 2, colspan=5)
+        #program.setCanvasBg("canvas", "black")
+        program.addCanvasImage("canvas", 35, 20, ImageTk.PhotoImage(file="../Resources/Images/lightning bolt.gif"))
+        program.hideCanvas("canvas")
 
     program.setFrameOverFunction("frame", [over, leave])
     meter()
