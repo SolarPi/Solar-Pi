9ad92f6cc17941f0ac02cdf6c35cfd6a42d7fbd4
The-Sleepy-Penguin <33980939+The-Sleepy-Penguin@users.noreply.github.com>
2018-06-06 18:18:11 +0100

    Start Coding now supports on the fly theme changing #19

--- Coding/Start Coding.py
+++ Coding/Start Coding.py
@@ -6,17 +6,111 @@ from appJar import gui
 from subprocess import Popen
 import webbrowser
 from SettingsRW import *
+from time import sleep
+from threading import Thread
 
-theme = getSetting("theme")
+theme1 = getSetting("theme")
 
-custom = theme == "Solar Pi"
+try:
+    from watchdog.observers import Observer
+    from watchdog.events import FileSystemEventHandler
+
+    class Watcher:
+        DIRECTORY_TO_WATCH = "/usr/local/bin/Solar Pi/Settings"  # Looks at settings
+
+        def __init__(self):
+            self.observer = Observer()
+
+        def run(self):
+            event_handler = Handler()
+            self.observer.schedule(event_handler, self.DIRECTORY_TO_WATCH, recursive=True)
+            self.observer.start()
+            try:
+                while True:
+                    sleep(5)
+            except:
+                self.observer.stop()
+                print("Error")
+
+            self.observer.join()
+
+
+    class Handler(FileSystemEventHandler):
+
+        @staticmethod
+        def on_any_event(event):
+            if event.is_directory:
+                return None
+
+            elif event.event_type == 'created':
+                # Take any action here when a file is first created.
+                print("Received created event")  # Triggered when a file is created
+
+            elif event.event_type == 'modified':
+                # Taken any action here when a file is modified.
+                print("Received modified event")
+                settings()  # Calls meter() when file is modified
+
+except:
+    pass
+
+def settings(first=False):
+    global app, theme1, theme2
+
+    theme2 = getSetting("theme")
+
+    if theme1 != theme2 or first == True:
+        if theme2 == "Solar Pi":
+            app.setTtkTheme("plastik")
+            app.setTtkTheme("clam")
+            # app.ttkStyle.configure(".", font="10")
+            # Highlighted button
+            app.ttkStyle.configure("H.TButton", background="#324581", foreground="white", bordercolor="#687396")
+            app.ttkStyle.map("H.TButton", background=[("pressed", "#172141"), ("active", "#4059a9")])
+
+            # Regular button
+            app.ttkStyle.configure("TButton", background="#dbdce2", bordercolor="#687396")
+
+            # Radiobutton
+            app.ttkStyle.map("TRadiobutton", background=[("active", "white")])
+
+            # LabelFrame
+            app.ttkStyle.configure("TLabelframe", bordercolor="#687396")
+
+            app.showButton("Go")
+            app.hideButton("go")
+
+            app.setBg("white")
+            app.ttkStyle.configure(".", foreground="black")
+
+        elif theme2 == "black":
+            app.setTtkTheme(theme2)
+            app.showButton("Go")
+            app.hideButton("go")
+
+        else:
+            app.setTtkTheme(theme2)
+            app.ttkStyle.map("TRadiobutton", background=[("active", "white")])
+            app.ttkStyle.configure(".", background="white", foreground="black")
+
+            app.showButton("go")
+            app.hideButton("Go")
+
+            app.setBg("white")
+            app.setFg("black")
+
+        app.setFont(family="piboto")
+        app.ttkStyle.configure(".", font=("piboto"))
+
+        theme1 = theme2
+
 
 # Handlers
 def ButtonHandler(press):
     option = app.getRadioButton("language")
     if press == "Exit ":
         quit()
-    elif press == "Go  ":
+    elif press.lower() == "go":
         if option == "Scratch (Easy)":
             Popen("/usr/local/bin/Solar Pi/Resources/Launchers/scratch launcher.sh")
         elif option == "Python (Intermedium/Hard)":
@@ -44,31 +138,6 @@ def ToolbarHandler(press):
 with gui("Start Coding", useTtk=True) as app:  # 400x320 # "420x290"
     app.setResizable(False)
 
-    if custom == True:
-        app.setTtkTheme("plastik")
-        app.setTtkTheme("clam")
-        #app.ttkStyle.configure(".", font="10")
-        # Highlighted button
-        app.ttkStyle.configure("H.TButton", background="#324581", foreground="white", bordercolor="#687396")
-        app.ttkStyle.map("H.TButton", background=[("pressed", "#172141"), ("active", "#4059a9")])
-
-        # Regular button
-        app.ttkStyle.configure("TButton", background="#dbdce2", bordercolor="#687396")
-
-        # Radiobutton
-        app.ttkStyle.map("TRadiobutton", background=[("active", "white")])
-
-        # LabelFrame
-        app.ttkStyle.configure("TLabelframe", bordercolor="#687396")
-    else:
-        app.setTtkTheme(theme)
-        if theme != "black":
-            app.ttkStyle.configure(".", background="white", foreground="black")
-            app.ttkStyle.map("TRadiobutton", background=[("active", "white")])
-
-    app.setFont(family="piboto")
-    app.ttkStyle.configure(".", font=("piboto"))
-
     # Menu
     #app.addMenuList("Applications", ["Solar Pi Home", "Leafpad"], MenuHandler)
     #app.addMenuList("IDEs", ["Scratch", "Python IDLE", "Thonny Python", "Java"], MenuHandler)
@@ -91,14 +160,37 @@ with gui("Start Coding", useTtk=True) as app:  # 400x320 # "420x290"
 
     with app.frame("frame"):
         app.setPadding(10, 10)
-        if custom == True or theme == "black":
-            app.addImageButton("Go  ", ButtonHandler, "../Resources/Images/go white.gif", 0, 0, align="right")
-            app.setButtonStyle("Go  ", "H.TButton")
+
+        app.addNamedButton("Go  ", "Go", ButtonHandler, 0, 0)
+        app.setButtonImage("Go", "../Resources/Images/go white.gif", align="right")
+        app.setButtonStyle("Go", "H.TButton")
+
+        app.addNamedButton("Go  ", "go", ButtonHandler, 0, 0)
+        app.setButtonImage("go", "../Resources/Images/go black.gif", align="right")
+        app.setButtonStyle("go", "H.TButton")
+
+        if theme1 == "Solar Pi" or theme1 == "black":
+            app.hideButton("go")
         else:
-            app.addImageButton("Go  ", ButtonHandler, "../Resources/Images/go black.gif", 0, 0, align="right")
-            app.setLabelFrameStyle("Choose a Programming Language:", "TFrame")
+            app.hideButton("Go")
+
+        #app.addImageButton("Go  ", ButtonHandler, "../Resources/Images/go white.gif", 0, 0, align="right")
+        #app.setButtonStyle("Go  ", "H.TButton")
+
+        #app.addImageButton("Go  ", ButtonHandler, "../Resources/Images/go black.gif", 0, 0, align="right")
+
+        app.setLabelFrameStyle("Choose a Programming Language:", "TFrame")
         app.addImageButton(" More Info  ", ButtonHandler, "../Resources/Images/more info.gif", 0, 1, align="right")
         app.addImageButton("Exit ", ButtonHandler, "../Resources/Images/cross.gif", 0, 2, align="right")
 
-    if theme != "black":
+    if theme1 != "black":
         app.setBg("white")
+
+    try:
+        w = Watcher()
+        t = Thread(target=w.run)
+        t.start()
+    except:
+        pass
+
+    settings(True)
